325f1f83b1611ac52fce9095811333af2e755d4c
Tim Dysinger <tim@dysinger.net>
2026-04-14 08:42:44 -0700

    openlibm: use empty __strong_alias on apple only

--- openlibm/include/openlibm_compat.h
+++ openlibm/include/openlibm_compat.h
@@ -1,9 +1,8 @@
 #ifndef OPENLIBM_COMPAT_H
 #define OPENLIBM_COMPAT_H
 
-#ifndef __strong_alias
-#define __strong_alias(new, old) \
-    extern typeof(old) new __attribute__((alias(#old)))
+#ifdef __APPLE__
+#define __strong_alias(new, old)
 #endif
 
 #endif
